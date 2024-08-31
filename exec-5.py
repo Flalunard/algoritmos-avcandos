510e18c47a0cb5e3067123729241caaea2762a85
Flavia kaline <luizarnaldolunardiperroni@gmail.com>
2024-08-31 09:27:55 -0300

    Create exec-5.py

---
+++ exec-5.py
@@ -0,0 +1,7 @@
+'''
+ 5 Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor.
+'''
+
+n=int(input ('Digite um número '))
+antecessor = ( n - 1) 
+print (' O antecessor é :' , antecessor   )
