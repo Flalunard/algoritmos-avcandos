a6465faf9e0552808a65af4651bb4f7b0ec79f96
Flavia kaline <luizarnaldolunardiperroni@gmail.com>
2024-08-31 09:30:05 -0300

    Create exec-6.py

---
+++ exec-6.py
@@ -0,0 +1,7 @@
+'''
+6) Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
+'''
+Alt=int(input('Altura: '))
+Ba=int(input('Base '))
+calc= Alt*Ba
+print(' As dimensões são ', calc, 'cm')
