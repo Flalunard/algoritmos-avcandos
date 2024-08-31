cd353014566eb25263b8062bd388a51c6541b6ad
Flavia kaline <luizarnaldolunardiperroni@gmail.com>
2024-08-31 09:25:49 -0300

    Create exec-3.py

---
+++ exec-3.py
@@ -0,0 +1,13 @@
+'''
+3 Os pares de instruções abaixo produzem o mesmo resultado? 
+'''
+
+A1 = (4/2) + (2/4)  
+A2 = 4/2 + 2/4
+B1 = 4/( 2 + 2 )/4
+B2 = 4/2 +  2/4  
+C1 = (4+2) * 2-4    
+C2 = 4+2 * 2-4
+print('O valor A1 vai dar : ', A1, ' O valor A2 vai dar: ' ,A2 )
+print('O valor B1 vai dar : ', B1, ' O valor B2 vai dar: ' ,B2 )
+print('O valor C1 vai dar : ', C1, ' O valor C2 vai dar: ' ,C2 )
